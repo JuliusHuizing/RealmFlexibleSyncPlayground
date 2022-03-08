f9be79d98e0fc404e3024ab02c241981579f4225
Julius Huizing <julius@Juliuss-MacBook-Pro.local>
2022-03-08 16:35:19 +0100

    Initial SwiftUI setup.

---
+++ frontend/RealmFlexibleSyncPlayground/RealmFlexibleSyncPlayground/RealmFlexibleSyncPlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealmFlexibleSyncPlaygroundApp.swift
+//  RealmFlexibleSyncPlayground
+//
+//  Created by Julius Huizing on 08/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct RealmFlexibleSyncPlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
