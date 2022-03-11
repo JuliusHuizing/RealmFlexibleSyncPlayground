6a653cc18ffd2c51cb9fa721402c347a964cb701
Julius Huizing <julius@Juliuss-MacBook-Pro.local>
2022-03-11 10:12:03 +0100

    Users can now unidirectionally send friends requests and accept them.

--- frontend/RealmFlexibleSyncPlayground/RealmFlexibleSyncPlayground/Views/Models/User.swift
+++ frontend/RealmFlexibleSyncPlayground/RealmFlexibleSyncPlayground/Views/Models/User.swift
@@ -19,6 +19,12 @@ class User: Object, ObjectKeyIdentifiable {
     @Persisted(primaryKey: true) var _id = UUID().uuidString
     @Persisted var userName = ""
     @Persisted var posts: List<UserPost>
+    
+    // TODO: Discuss whether this is an antipattern: prob not safe to store other ID's like this? + Likely should be embedded objects since in practice it are bounded sets?
+    @Persisted var friends: List<String> // List of string UUID's associated with friends.
+    @Persisted var friendRequestsIncoming: List<String> // similar to above
+    @Persisted var friendRequestsOutgoing: List<String> // similar to above
+
 
     convenience init(userName: String, id: String) {
         self.init()
