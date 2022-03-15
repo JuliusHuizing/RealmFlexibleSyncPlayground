ef82f5a97eefba86c62061b9b3595655a8944fea
Julius Huizing <julius@Juliuss-MacBook-Pro.local>
2022-03-15 10:22:38 +0100

    DailyDiaryView now uses nav links instead of sheet to present detailDiaryEntryView to avoid unexpected nil-valued observable real objc.

--- frontend/RealmFlexibleSyncPlayground/RealmFlexibleSyncPlayground/Views/DailyDiaryView.swift
+++ frontend/RealmFlexibleSyncPlayground/RealmFlexibleSyncPlayground/Views/DailyDiaryView.swift
@@ -13,22 +13,33 @@ struct DailyDiaryView: View {
     @ObservedRealmObject var user: User
     @ObservedResults(DiaryEntry.self) var diaryEntries
     @State private var showingSheet = false
-    @State private var selectedDiaryEntry: DiaryEntry = DiaryEntry()
+    @State private var selectedDiaryEntry: DiaryEntry?
+    var todaysEntries: [DiaryEntry]? {
+        diaryEntries.filter({Calendar.current.isDate(date, equalTo: $0.date, toGranularity: .day)})
+       }
     let date: Date
     var body: some View {
+        NavigationView {
         VStack {
             List {
-                ForEach(diaryEntries.filter({Calendar.current.isDate(date, equalTo: $0.date, toGranularity: .day)})) { entry in
-                    VStack{
-                        Text(entry.title)
-                        Text(entry.body)
-                        Button("Edit") {
-                            //  TODO: After pressing this button on a just created diary entry (see button below), I would expect the DetailDiaryView to immediately show a diary entry with title 'test title' ad body 'test body'. Instead, it often first shows empty strings, and only shows the expected strings after multiple attempts or after adding more diary entries with the button below.
-                            selectedDiaryEntry = entry
-                            showingSheet = true
+                if let todaysEntries = todaysEntries {
+                    ForEach(todaysEntries, id: \.self) { entry in
+                        VStack{
+                            Text(entry.title)
+                            Text(entry.body)
+//                            Button("Edit") {
+                                //  TODO: After pressing this button on a just created diary entry (see button below), I would expect the DetailDiaryView to immediately show a diary entry with title 'test title' ad body 'test body'. Instead, it often first shows empty strings, and only shows the expected strings after multiple attempts or after adding more diary entries with the button below.
+                                NavigationLink("edit", destination:DiaryEntryDetailView(user: user, diaryEntry: entry))
+//                                selectedDiaryEntry = entry
+//                                if selectedDiaryEntry != nil {
+//                                    showingSheet = true
+//                                }
+//
+//                            }
                         }
                     }
                 }
+              
             }
             Button("Add Entry to Diary") {
                 let initDiaryEntry = DiaryEntry(ownerID: user._id)
@@ -37,8 +48,13 @@ struct DailyDiaryView: View {
                 initDiaryEntry.body = "test body"
                 $diaryEntries.append(initDiaryEntry)
             }
-        }.sheet(isPresented: $showingSheet, onDismiss: {}) {
-            DiaryEntryDetailView(user: user, diaryEntry: selectedDiaryEntry)
+        }}.sheet(isPresented: $showingSheet, onDismiss: {}) {
+            DiaryEntryDetailView(user: user, diaryEntry: selectedDiaryEntry!)
+//                .onAppear(perform: {
+////                    todaysDiaryEntires = diaryEntries.filter{
+////                        Calendar.current.isDate(date, equalTo: $0.date, toGranularity: .day)
+//                    }
+//                })
         }
     }
 }
